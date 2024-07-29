1417ffc7a875fef4a87624ba2fe7934f8850d86f
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-07-29 13:04:03 -0400

    proyecto base Microservicio Productos

---
+++ src/main/java/cl/edu/lite/thinking/products/ListaProductosMicroservicioResTfulApplication.java
@@ -0,0 +1,13 @@
+package cl.edu.lite.thinking.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaProductosMicroservicioResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaProductosMicroservicioResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/edu/lite/thinking/products/ListaProductosMicroservicioResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.edu.lite.thinking.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaProductosMicroservicioResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
